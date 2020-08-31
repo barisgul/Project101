c296b72a48beea836756ec89d629191ba278d1b6
Barış <bariss.gull@gmail.com>
2020-08-31 15:12:56 +0300

    Update StaticClassWrapper.cs

--- Project101/StaticClassWrapper.cs
+++ Project101/StaticClassWrapper.cs
@@ -6,7 +6,7 @@ namespace Project101
 {
     public class StaticClassWrapper : IStaticClassWrapper
     {
-        
+        //oluşturduğumuz bu wrap edilmiş metot ile artık mocklanan IStaticClassWrapper interfaceine ait bir stub yaratabilir ve statik classı izole edebiliriz 
         public string WriteMessage(string message)
         {
             throw new NotImplementedException();
