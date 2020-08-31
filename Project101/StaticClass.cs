ae91b9a013ed547760285b5d1f12dda38b1b6fd5
Barış <bariss.gull@gmail.com>
2020-08-31 15:12:02 +0300

    Update StaticClass.cs

--- Project101/StaticClass.cs
+++ Project101/StaticClass.cs
@@ -6,7 +6,8 @@ namespace Project101
 {
     public static class StaticClass
     {
-
+        // bu statik metod test edilecek bir başka metot içinde kullanıldığında mocklanamayacağı için bir ara katmana ihtiyaç duyulacaktır. bunun için interfaceten türeyen 
+        // bir wrapper class oluşturmak yeterli olacaktır. 
         public static string WriteMessage(string message)
         {
             return message;
